541c867bc1309f384b37c1d6d41173ebb3d81c4a
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-04-13 20:36:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# seenwhat
---
+++ extension/popup.js
@@ -0,0 +1,29 @@
+function getTabInfo(callback) {
+
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    var title = tab.title;
+    var favIconUrl = tab.favIconUrl;
+
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+    console.assert(typeof title == 'string', 'tab.title should be a string');
+    console.assert(typeof favIconUrl == 'string', 'tab.favIconUrl should be a string');
+
+    callback(url, title, favIconUrl);
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getTabInfo(function(url, title, favIconUrl) {
+    document.getElementById('link').textContent = title;
+    document.getElementById('link').href = title;
+    document.getElementById('favicon').src = favIconUrl;
+  });
+});
